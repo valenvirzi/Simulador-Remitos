a778d1d2a8dccee29dc94a6a0ecd81e368ba1e5c
Valentin Virzi <v.virzi@hotmail.com>
2023-07-19 17:38:32 -0300

    starting js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let ivaToggle, paidToggle, owesToggle, prevDebtToggle, paymentH2, paymentAmount, debtH2, debtAmount, subtotalAmount, ivaAmount, previousDebtAmount, totalAmount, btnAddProduct;
+ivaToggle = document.getElementById("iva");
+paidToggle = document.getElementById("paid");
+owesToggle = document.getElementById("owes");
+prevDebtToggle = document.getElementById("prevDebt");
+paymentH2 = document.getElementById("payment");
+paymentAmount = document.getElementById("paymentAmount");
+debtH2 = document.getElementById("debt");
+debtAmount = document.getElementById("debtAmount");
+subtotalAmount = document.getElementById("subtotalAmount");
+ivaAmount = document.getElementById("ivaAmount");
+previousDebtAmount = document.getElementById("previousDebtAmount");
+totalAmount = document.getElementById("totalAmount");
+btnAddProduct = document.getElementById("btnAddProduct");
