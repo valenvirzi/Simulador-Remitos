55ff13343af31f8d8a97b41c2de3a6b6d847d465
Valentin Virzi <v.virzi@hotmail.com>
2023-07-19 22:45:55 -0300

    added container for inputs

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ ivaToggle = document.getElementById("iva");
 paidToggle = document.getElementById("paid");
 owesToggle = document.getElementById("owes");
 prevDebtToggle = document.getElementById("prevDebt");
-paymentH2 = document.getElementById("payment");
+paymentH2 = document.getElementById("paymentH2");
 paymentAmount = document.getElementById("paymentAmount");
-debtH2 = document.getElementById("debt");
+debtH2 = document.getElementById("debtH2");
 debtAmount = document.getElementById("debtAmount");
 subtotalAmount = document.getElementById("subtotalAmount");
 ivaAmount = document.getElementById("ivaAmount");
 previousDebtAmount = document.getElementById("previousDebtAmount");
 totalAmount = document.getElementById("totalAmount");
 btnAddProduct = document.getElementById("btnAddProduct");
+
