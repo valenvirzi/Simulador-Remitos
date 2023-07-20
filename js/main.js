f43f0822daad2f4c4b5910313ce97767c5902ff4
Valentin Virzi <v.virzi@hotmail.com>
2023-07-19 23:43:08 -0300

    taking a break

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-let ivaToggle, paidToggle, owesToggle, prevDebtToggle, paymentH2, paymentAmount, debtH2, debtAmount, subtotalAmount, ivaAmount, previousDebtAmount, totalAmount, btnAddProduct;
+let containerDiv,
+  ivaToggle,
+  paidToggle,
+  owesToggle,
+  prevDebtToggle,
+  paymentH2,
+  paymentAmount,
+  debtH2,
+  debtAmount,
+  subtotalAmount,
+  ivaAmount,
+  previousDebtAmount,
+  totalAmount,
+  btnAddProduct;
+containerDiv = document.getElementById("containerDiv");
 ivaToggle = document.getElementById("iva");
 paidToggle = document.getElementById("paid");
 owesToggle = document.getElementById("owes");
@@ -13,3 +27,38 @@ previousDebtAmount = document.getElementById("previousDebtAmount");
 totalAmount = document.getElementById("totalAmount");
 btnAddProduct = document.getElementById("btnAddProduct");
 
+
+
+// class Item {
+//   constructor(itemQuantity, itemName, itemPrice, itemSubtotal) {
+//     this.itemQuantity = itemQuantity;
+//     this.itemName = itemName;
+//     this.itemPrice = itemPrice;
+//     itemSubtotal = this.multiply(itemQuantity, itemPrice);
+//   }
+
+//   multiply(itemQuantity, itemPrice) {
+//     let itemSubtotal = itemQuantity * itemPrice;
+//     return itemSubtotal;
+//   }
+
+// }
+
+// let prod = document.createElement("div");
+// prod.classList.add("main__products-div");
+// prod.innerHTML = `<input class="main__products-item" 
+// type="number"
+// placeholder="Cantidad">
+// <input class="main__products-item" 
+// type="text"
+// placeholder="Producto">
+// <input class="main__products-item" 
+// type="number"
+// placeholder="Precio">
+// <span class="main__products-item main__products-item-span">$</span>`;
+
+// function addChild(prod) {
+//     containerDiv.appendChild(prod);
+// }
+
+// btnAddProduct.addEventListener("click", addChild(prod))
